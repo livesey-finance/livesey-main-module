906bcda9b2ca3fa0926f2bac74bd6cafdd50163a
hhuziuk <huziukwork@gmail.com>
2024-08-18 20:25:16 +0200

    changed strategy in backend/core/domain

--- backend/configs/corsConfig.js
+++
@@ -1,7 +0,0 @@
-export const corsConfig = {
-  // origin: ['https://trusteddomain.com', 'https://anothertrusteddomain.com'],
-  origin: ["*"], // HAVE TO BE CHANGED
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
